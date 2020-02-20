c9ef00e9426583878b5c4ef1318ca7207ccb106e
adjiakbar18 <ajiakbar420@gmail.com>
2020-02-20 15:31:14 +0700

    modif route

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
 
     public $successStatus = 200;
 
-    public function login(){
+    public function login(Request $request){
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
             $user = Auth::user();
             $success['token'] =  $user->createToken('nApp')->accessToken;
--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::post('login', 'API\UserController@login');
-Route::post('register', 'API\UserController@register');
+Route::post('/login', 'API\UserController@login');
+Route::post('/register', 'API\UserController@register');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
